5a1b506fe9e035f52d1c894b35ec0d70b1ab1f6b
willworks <scnuzhonghanjin@gmail.com>
2015-11-19 15:43:57 +0800

    scope都遵循原型继承，子代可以访问父级scope

--- res/js/app.js
+++ res/js/app.js
@@ -1,5 +1,13 @@
-var app = angular.module('ap', []);
-app.controller('ctr', function($scope) {
-    $scope.name= "John";
-    $scope.age= "Doe";
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope) {
+    $scope.name = 'John';
+    $scope.age = 'Doe';
+});
+app.controller('parent', function($scope) {
+	$scope.person = 'zhong';
+});
+app.controller('children', function($scope) {
+	$scope.sayHello = function() {
+		$scope.person = 'hanjin';
+	};
 });
