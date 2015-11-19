2df8c26f983c934af07ec4c21333d9cf654d1a28
willworks <scnuzhonghanjin@gmail.com>
2015-11-19 15:59:03 +0800

    双向数据绑定互动操作

--- res/js/app.js
+++ res/js/app.js
@@ -7,7 +7,16 @@ app.controller('parent', function($scope) {
 	$scope.person = 'zhong';
 });
 app.controller('children', function($scope) {
-	$scope.sayHello = function() {
+	$scope.sayHello = function($scope) {
 		$scope.person = 'hanjin';
+	};
+});
+app.controller('cal', function($scope) {
+	$scope.num = 0;
+	$scope.add = function(){
+		$scope.num++;
+	};
+	$scope.sub = function(){
+		$scope.num--;
 	};
 });
